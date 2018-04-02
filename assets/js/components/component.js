c3a59f67a51747e83e419d9a67973d3253619d13
RIGHTPOINT\TFinkle <tfinkle@rightpoint.com>
2018-04-02 14:52:09 -0400

    initial commit

--- assets/js/components/component.js
+++ assets/js/components/component.js
@@ -17,9 +17,23 @@ module.exports = function() {
 		
 		bindUI: function() {
 			
-			$('.box').click(function() {
-				$(this).toggleClass('open');
-			});
+		let bkg = require("./json/backgrounds.json");
+		let bit = require("./json/basic-items.json");
+		let cls = require("./json/classes.json");
+		let fet = require("./json/feats.json");
+		let itm = require("./json/items.json");
+		let rce = require("./json/races.json");
+		
+		let backgrounds = bkg.background;
+		let basics = bit.basicitem;
+		let properties = bit.itemProperty;
+		let itemTypes = bit.itemType;
+		let classes = cls.class;
+		let feats = fet.feat;
+		let items = itm.item;
+		let races = rce.race;
+		
+		console.log(bkg);
 						
 		}
 	}
